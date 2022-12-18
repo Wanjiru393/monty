8cb2956d8703f06cbcc19c407de8d508d43e4e85
Wanjiru393 <carolciru393@gmail.com>
2022-12-18 12:17:47 -0800

    Adding implementation of pull and pall opcodes

--- monty.c
+++ monty.c
@@ -1,32 +1,24 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include "monty.h"
+global_var var_global;
+/**
+ * main - driver function for monty program
+ * @ac: int num of arguments
+ * @av: opcode file
+ * Return: 0
+ */
+int main(int ac, char **av)
+{
+	stack_t *stack;
 
-typedef struct stack {
-    int value;
-    struct stack *next;
-} stack;
+	stack = NULL;
+	if (ac != 2)
+	{
+		fprintf(stderr, "USAGE: monty file\n");
+		exit(EXIT_FAILURE);
+	}
 
-void push(stack **head, int value) {
-    stack *new_node = malloc(sizeof(stack));
-    new_node->value = value;
-    new_node->next = *head;
-    *head = new_node;
+	read_file(av[1], &stack);
+    /* recordar liberar memorias */
+	free_dlistint(stack);
+	return (0);
 }
-
-void pall(stack *head) {
-    stack *current = head;
-    while (current != NULL) {
-        printf("%d ", current->value);
-        current = current->next;
-    }
-}
-
-int main() {
-    stack *head = NULL;
-    push(&head, 1);
-    push(&head, 2);
-    push(&head, 3);
-    pall(head);  // prints 3, 2, 1
-    return 0;
-}
-
